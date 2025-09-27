3c45e1a2408411c2686fda112b845891bec6306e
Ngoc Nhan <60886911+petertran410@users.noreply.github.com>
2025-09-27 12:49:07 +0700

    remove error module

--- src/client_user/client_user.module.ts
+++ src/client_user/client_user.module.ts
@@ -1,13 +1,24 @@
 import { Module } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';
+import { ConfigModule } from '@nestjs/config';
 import { ClientUserService } from './client_user.service';
 import { ClientUserController } from './client_user.controller';
 import { PrismaModule } from '../prisma/prisma.module';
 import { ProductModule } from '../product/product.module';
+import { KiotVietService } from '../kiotviet/kiotviet.service';
 
 @Module({
-  imports: [PrismaModule, ProductModule],
+  imports: [
+    PrismaModule,
+    ProductModule,
+    HttpModule.register({
+      timeout: 60000,
+      maxRedirects: 10,
+    }),
+    ConfigModule,
+  ],
   controllers: [ClientUserController],
-  providers: [ClientUserService],
+  providers: [ClientUserService, KiotVietService],
   exports: [ClientUserService],
 })
 export class ClientUserModule {}
