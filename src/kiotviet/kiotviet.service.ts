6dea5610398b11015c8a62e0b4863eea2e765ec2
Ngoc Nhan <60886911+petertran410@users.noreply.github.com>
2025-10-26 19:50:25 +0700

    Update data upsert

--- src/kiotviet/kiotviet.service.ts
+++ src/kiotviet/kiotviet.service.ts
@@ -752,7 +752,7 @@ export class KiotVietService {
 
   private async forwardRawWebhookData(rawWebhookData: any): Promise<void> {
     const endpoints = [
-      'https://n8n.hisweetievietnam.com/webhook/webhook-kiotviet-website',
+      'https://n8n.hisweetievietnam.com/webhook-test/webhook-kiotviet-website',
       'https://kiot.hisweetievietnam.com/webhook/order',
       'https://n8n.hisweetievietnam.com/webhook/take-order-webhook-from-kiotviet',
     ];
